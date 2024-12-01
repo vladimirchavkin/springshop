088e988a5600bd19fad56138cb7f687b7bae17bc
vladimirchavkin <vladimirchavkinwork@gmail.com>
2024-12-01 17:37:49 +0300

    SS-01-FIX: Validation has been migrated to catalogue-service

--- manager-service/src/main/java/ru/chavkin/gp/managerservice/controller/payload/UpdateProductPayload.java
+++ manager-service/src/main/java/ru/chavkin/gp/managerservice/controller/payload/UpdateProductPayload.java
@@ -1,20 +1,8 @@
 package ru.chavkin.gp.managerservice.controller.payload;
 
 
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
-
 /**
  * @author Vladimir Chavkin (vladimirchavkinwork@gmail.com)
  */
 
-public record UpdateProductPayload(
-
-        @NotNull(message = "{catalogue.products.update.errors.title_is_null}")
-        @Size(min = 3, max = 50, message = "{catalogue.products.update.errors.title_size_is_invalid}")
-        String title,
-
-        @Size(max = 500, message = "{catalogue.products.create.update.details_size_is_invalid}")
-        String details
-) {
-}
+public record UpdateProductPayload(String title, String details) {}
