c982f6c2aaf74e04a4dad252d8ee7b192ad0bda3
vladimirchavkin <vladimirchavkinwork@gmail.com>
2024-12-13 15:01:40 +0300

    Revert "SS-03-DB-FIX: upd sql schema"
    This reverts commit 314d811f15cdbcee88aab1759cff1518af5371a1.

--- manager-service/src/main/resources/db/migration/V24_1_1__Basic_schema.sql
+++ manager-service/src/main/resources/db/migration/V24_1_1__Basic_schema.sql
@@ -1,17 +1,17 @@
 create schema if not exists user_management;
 
-create table if not exists user_management.t_user (
+create table user_management.t_user (
     id serial primary key,
     c_username varchar not null check ( length(trim(c_username)) > 0 ) unique,
     c_password varchar
 );
 
-create table if not exists user_management.t_authority (
+create table user_management.t_authority (
     id serial primary key,
     c_authority varchar not null check ( length(trim(c_authority)) > 0 ) unique
 );
 
-create table if not exists user_management.t_user_authority(
+create table user_management.t_user_authority(
     id serial primary key,
     id_user int not null references user_management.t_user(id),
     id_authority int not null references  user_management.t_authority(id),
